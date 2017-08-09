ed126763cf78391b28d58cd7deca72cc7dd33c28
John <john.wong.h@gmail.com>
2017-08-09 00:12:46 +0000

    Added feature spec code to test add to cart function.

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  
+  scenario "Cart gets updated when add to cart button is pressed" do
+    visit '/'
+    expect(page).to have_content 'My Cart (0)'
+
+    first("article.product").find_link("Add").click
+    expect(page).to have_content 'My Cart (1)'
+  end
 end
